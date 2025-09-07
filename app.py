951cf5481eca420068ee0be81679b272ff79ddda
swapnil <swapnil@filmboardmovies.com>
2025-09-07 19:31:32 +0800

    added notebook

--- app.py
+++ app.py
@@ -311,4 +311,5 @@ if __name__ == '__main__':
     print("🌐 Open your browser and go to: http://localhost:5000")
     print("="*50)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
